ba17d328630e6dce784298fb34918879a91c10b1
sarahm <minutemin@gmail.com>
2023-06-21 16:49:51 -0700

    write get, delete code in products routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
     });
-    res.status(200).json(categories);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
